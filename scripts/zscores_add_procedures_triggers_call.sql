8b2857a357ade0d2cd425e2101b57b5029680260
Collin Lacoursiere <61947934+cl16@users.noreply.github.com>
2025-03-10 18:26:49 -0400

    update id column names to snake case

--- scripts/zscores_add_procedures_triggers_call.sql
+++ scripts/zscores_add_procedures_triggers_call.sql
@@ -21,21 +21,21 @@ DECLARE r_score INT;
 SELECT COUNT(*) FROM publication INTO n;
 SET i=0;
 WHILE i < n DO
-	SET pid = (SELECT pubId FROM publication LIMIT i,1);
-    SET p_avg = (SELECT ROUND(AVG(review.score), 2) FROM review WHERE review.pubId = pid);
-    SET p_stddev = (SELECT ROUND(STDDEV_POP(review.score), 2) FROM review WHERE review.pubId = pid);
-    UPDATE publication SET publication.score_avg = p_avg WHERE publication.pubId = pid;
-    UPDATE publication SET publication.score_stddev = p_stddev WHERE publication.pubId = pid;
-    SELECT COUNT(*) FROM (SELECT gameId FROM review WHERE review.pubId = pid) AS temp INTO gn;
+	SET pid = (SELECT pub_id FROM publication LIMIT i,1);
+    SET p_avg = (SELECT ROUND(AVG(review.score), 2) FROM review WHERE review.pub_id = pid);
+    SET p_stddev = (SELECT ROUND(STDDEV_POP(review.score), 2) FROM review WHERE review.pub_id = pid);
+    UPDATE publication SET publication.score_avg = p_avg WHERE publication.pub_id = pid;
+    UPDATE publication SET publication.score_stddev = p_stddev WHERE publication.pub_id = pid;
+    SELECT COUNT(*) FROM (SELECT game_id FROM review WHERE review.pub_id = pid) AS temp INTO gn;
     SET j = 0;
     WHILE j < gn DO
         SET gid = (
-            SELECT gameId FROM review WHERE review.pubId = pid
+            SELECT game_id FROM review WHERE review.pub_id = pid
             LIMIT j,1
         );
-        SET r_score = (SELECT score FROM review WHERE (review.pubId = pid) AND (review.gameId = gid));
+        SET r_score = (SELECT score FROM review WHERE (review.pub_id = pid) AND (review.game_id = gid));
         SET g_zscore = IF(p_stddev > 0, ROUND((r_score - p_avg) / p_stddev, 3), IF(r_score = p_avg, 0, NULL));
-        INSERT INTO stat (gameId, pubId, zscore)
+        INSERT INTO stat (game_id, pub_id, zscore)
 			VALUES (gid, pid, g_zscore) 
 			ON DUPLICATE KEY UPDATE zscore = g_zscore;
         SET j = j + 1;
@@ -59,20 +59,20 @@ DECLARE j INT DEFAULT 0;
 DECLARE gid INT;
 DECLARE g_zscore DECIMAL(4,3);
 DECLARE r_score INT;
-SET p_avg = (SELECT ROUND(AVG(review.score), 2) FROM review WHERE review.pubId = pid);
-SET p_stddev = (SELECT ROUND(STDDEV_POP(review.score), 2) FROM review WHERE review.pubId = pid);
-UPDATE publication SET publication.score_avg = p_avg WHERE publication.pubId = pid;
-UPDATE publication SET publication.score_stddev = p_stddev WHERE publication.pubId = pid;
-SELECT COUNT(*) FROM (SELECT gameId FROM review WHERE review.pubId = pid) AS temp INTO gn;
+SET p_avg = (SELECT ROUND(AVG(review.score), 2) FROM review WHERE review.pub_id = pid);
+SET p_stddev = (SELECT ROUND(STDDEV_POP(review.score), 2) FROM review WHERE review.pub_id = pid);
+UPDATE publication SET publication.score_avg = p_avg WHERE publication.pub_id = pid;
+UPDATE publication SET publication.score_stddev = p_stddev WHERE publication.pub_id = pid;
+SELECT COUNT(*) FROM (SELECT game_id FROM review WHERE review.pub_id = pid) AS temp INTO gn;
 SET j = 0;
 WHILE j < gn DO
 	SET gid = (
-		SELECT gameId FROM review WHERE review.pubId = pid
+		SELECT game_id FROM review WHERE review.pub_id = pid
 		LIMIT j,1
 	);
-	SET r_score = (SELECT score FROM review WHERE (review.pubId = pid) AND (review.gameId = gid));
+	SET r_score = (SELECT score FROM review WHERE (review.pub_id = pid) AND (review.game_id = gid));
 	SET g_zscore = IF(p_stddev > 0, ROUND((r_score - p_avg) / p_stddev, 3), IF(r_score = p_avg, 0, NULL));
-	UPDATE stat SET stat.zscore = g_zscore WHERE (stat.pubId = pid) AND (stat.gameId = gid);
+	UPDATE stat SET stat.zscore = g_zscore WHERE (stat.pub_id = pid) AND (stat.game_id = gid);
 	SET j = j + 1;
 END WHILE;
 END;
@@ -82,45 +82,45 @@ DELIMITER ;
 
 -- TRIGGER: After INSERT on review, insert the game-publication row into stat table, then
 -- call update procedure on publication and its stats/zscores
-DROP TRIGGER IF EXISTS review_insert_pubStatUpdate;
+DROP TRIGGER IF EXISTS review_insert_pub_stat_update;
 DELIMITER //
-CREATE TRIGGER review_insert_pubStatUpdate AFTER INSERT ON review
+CREATE TRIGGER review_insert_pub_stat_update AFTER INSERT ON review
 	FOR EACH ROW
     BEGIN
-		INSERT INTO stat VALUES (NEW.gameId, NEW.pubId, NULL);
-		CALL calc_stats_by_pub(NEW.pubId);
+		INSERT INTO stat VALUES (NEW.game_id, NEW.pub_id, NULL);
+		CALL calc_stats_by_pub(NEW.pub_id);
 	END//
 DELIMITER ;
 
 
 -- TRIGGER: After UPDATE on review, call update procedure on publication and its stats/zscores
-DROP TRIGGER IF EXISTS review_update_pubStatUpdate;
+DROP TRIGGER IF EXISTS review_update_pub_stat_update;
 DELIMITER //
-CREATE TRIGGER review_update_pubStatUpdate AFTER UPDATE ON review
+CREATE TRIGGER review_update_pub_stat_update AFTER UPDATE ON review
 	FOR EACH ROW
     BEGIN
-		CALL calc_stats_by_pub(NEW.pubId);
+		CALL calc_stats_by_pub(NEW.pub_id);
 	END//
 DELIMITER ;
 
 
 -- TRIGGER: Before DELETE on review, delete the corresponding row in stat table
-DROP TRIGGER IF EXISTS review_beforeDelete_pubStatUpdate;
+DROP TRIGGER IF EXISTS review_before_delete_pub_stat_update;
 DELIMITER //
-CREATE TRIGGER review_beforeDelete_pubStatUpdate BEFORE DELETE ON review
+CREATE TRIGGER review_before_delete_pub_stat_update BEFORE DELETE ON review
 	FOR EACH ROW
     BEGIN
-		DELETE FROM stat WHERE stat.gameId = OLD.gameId AND stat.pubId = OLD.pubId;
+		DELETE FROM stat WHERE stat.game_id = OLD.game_id AND stat.pub_id = OLD.pub_id;
 	END//
 DELIMITER ;
 
 
 -- TRIGGER: After DELETE on review, call update procedure on publication and its stats/zscores
-DROP TRIGGER IF EXISTS review_afterDelete_pubStatUpdate;
+DROP TRIGGER IF EXISTS review_afterDelete_pub_stat_update;
 DELIMITER //
-CREATE TRIGGER review_afterDelete_pubStatUpdate AFTER DELETE ON review
+CREATE TRIGGER review_afterDelete_pub_stat_update AFTER DELETE ON review
 	FOR EACH ROW
     BEGIN
-		CALL calc_stats_by_pub(OLD.pubId);
+		CALL calc_stats_by_pub(OLD.pub_id);
 	END//
 DELIMITER ;
--- scripts/zscores_creation_script.sql
+++ scripts/zscores_creation_script.sql
@@ -18,10 +18,10 @@ USE `zscores` ;
 -- Table `zscores`.`game`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zscores`.`game` (
-  `gameId` INT NOT NULL AUTO_INCREMENT,
+  `game_id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`gameId`),
-  UNIQUE INDEX `gameId_UNIQUE` (`gameId` ASC) VISIBLE)
+  PRIMARY KEY (`game_id`),
+  UNIQUE INDEX `game_id_UNIQUE` (`game_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -29,14 +29,14 @@ ENGINE = InnoDB;
 -- Table `zscores`.`publication`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zscores`.`publication` (
-  `pubId` INT NOT NULL AUTO_INCREMENT,
+  `pub_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `score_avg` DECIMAL(5,2) NULL,
   CHECK (`score_avg` >= 0 AND `score_avg` <= 100),
   `score_stddev` DECIMAL(4,2) NULL,
   CHECK (`score_stddev` >= 0),
-  PRIMARY KEY (`pubId`),
-  UNIQUE INDEX `pubId_UNIQUE` (`pubId` ASC) VISIBLE)
+  PRIMARY KEY (`pub_id`),
+  UNIQUE INDEX `pub_id_UNIQUE` (`pub_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -44,21 +44,21 @@ ENGINE = InnoDB;
 -- Table `zscores`.`review`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zscores`.`review` (
-  `gameId` INT NOT NULL,
-  `pubId` INT NOT NULL,
+  `game_id` INT NOT NULL,
+  `pub_id` INT NOT NULL,
   `date` DATETIME NULL,
   `score` INT NOT NULL,
   CHECK (`score` >= 0 AND `score` <= 100),
-  PRIMARY KEY (`gameId`, `pubId`),
-  INDEX `fk_score_publication1_idx` (`pubId` ASC) VISIBLE,
+  PRIMARY KEY (`game_id`, `pub_id`),
+  INDEX `fk_score_publication1_idx` (`pub_id` ASC) VISIBLE,
   CONSTRAINT `fk_score_game`
-    FOREIGN KEY (`gameId`)
-    REFERENCES `zscores`.`game` (`gameId`)
+    FOREIGN KEY (`game_id`)
+    REFERENCES `zscores`.`game` (`game_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_score_publication1`
-    FOREIGN KEY (`pubId`)
-    REFERENCES `zscores`.`publication` (`pubId`)
+    FOREIGN KEY (`pub_id`)
+    REFERENCES `zscores`.`publication` (`pub_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -68,13 +68,13 @@ ENGINE = InnoDB;
 -- Table `zscores`.`stat`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `zscores`.`stat` (
-  `gameId` INT NOT NULL,
-  `pubId` INT NOT NULL,
+  `game_id` INT NOT NULL,
+  `pub_id` INT NOT NULL,
   `zscore` DECIMAL(4,3) NULL,
-  PRIMARY KEY (`gameId`, `pubId`),
+  PRIMARY KEY (`game_id`, `pub_id`),
   CONSTRAINT `fk_stat_gamePublication`
-    FOREIGN KEY (`gameId`, `pubId`)
-    REFERENCES `zscores`.`review` (`gameId`, `pubId`)
+    FOREIGN KEY (`game_id`, `pub_id`)
+    REFERENCES `zscores`.`review` (`game_id`, `pub_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
